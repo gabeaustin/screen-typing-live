204645a45532aad79db8618a3a6ec04601e2bcfd
gabeaustin <gabriel.austin2019@gmail.com>
2021-10-10 14:44:44 -0500

    added js to make typing function work

--- main.js
+++ main.js
@@ -2,4 +2,17 @@ let myText = "Hi! I'm Typo, and I'll be your screen typist for today!";
 let textArray = myText.split("");
 let loopTimer;
 
+function frameLooper() {
+  if (textArray.length > 0) {
+    document.getElementById("type_text").innerHTML += textArray.shift();
+  } else {
+    clearTimeout(loopTimer);
+    return false;
+  }
+
+  loopTimer = setTimeout("frameLooper()", 80);
+}
+
+frameLooper();
+
 // document.write(textArray);
